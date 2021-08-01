6577a3fbad48fdbaabf2195b1af19ab9d6f898a2
ikizey <ikizey@icloud.com>
2021-08-01 15:20:01 +0300

    Initial Commit

---
+++ Stocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks
+//
+//  Created by PrincePhoenix on 01.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by PrincePhoenix on 01.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
