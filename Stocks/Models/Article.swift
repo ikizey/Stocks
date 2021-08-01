0ddd87b35e3c78c3528567c6b599e59c5b03d98c
ikizey <ikizey@icloud.com>
2021-08-01 18:00:55 +0300

    add Article, add getTopNews to Webservice

---
+++ Stocks/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  Stocks
+//
+//  Created by PrincePhoenix on 01.08.2021.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let imageURL: String
+    let publication: String
+}
--- Stocks/Services/Webservice.swift
+++ Stocks/Services/Webservice.swift
@@ -9,18 +9,41 @@ import Foundation
 
 class Webservice {
     
+    func getTopNews(completion: @escaping (([Article]?) -> Void)) {
+        let urlString = "https://island-bramble.glitch.me/top-news"
+        guard let url = URL(string: urlString) else {
+            fatalError("Wrong URL!")}
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            let articles = try? JSONDecoder().decode([Article].self, from: data)
+            DispatchQueue.main.async {
+                completion(articles)
+            }
+        }.resume()
+    }
+    
     func getStocks(completion: @escaping (([Stock]?) -> Void)) {
-    let urlString = "https://island-bramble.glitch.me/stocks"
+        let urlString = "https://island-bramble.glitch.me/stocks"
         guard let url = URL(string: urlString) else {
             fatalError("Wrong URL!")}
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
-                completion(nil)
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
                 return
             }
             let stocks = try? JSONDecoder().decode([Stock].self, from: data)
-            stocks == nil ? completion(nil) : completion (stocks)
+            DispatchQueue.main.async {
+                completion(stocks)
+            }
         }.resume()
     }
 }
